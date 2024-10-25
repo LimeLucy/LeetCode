1b9ed52e0f5c5ef3911c1f6f978a1072b288126d
Lucy <love_eunji@naver.com>
2024-10-25 17:44:11 +0900

    add

---
+++ Q/0600_0699/Quest_670.cs
@@ -0,0 +1,97 @@
+﻿using System.Linq;
+
+namespace LeetCode.Q
+{
+	internal class Quest_670 : Quest
+	{
+		#region 문제
+		/*
+		정수가 주어집니다.최대 한 번에 두 자리까지 바꿀 수 있으며 최대값을 얻을 수 있습니다.얻을 수 있는 최대값을 반환합니다.
+
+		Example 1:
+			Input: num = 2736
+			Output: 7236
+			Explanation: Swap the number 2 and the number 7.
+
+		Example 2:
+			Input: num = 9973
+			Output: 9973
+			Explanation: No swap.
+		*/
+
+		/*
+		 * Runtime 5ms / Beats 100.00%  (Yay!)
+		 * Memory 28.40MB / Beats 6.38% (.ㅠㅠ...) 
+		 */
+		#endregion
+
+		public override void Init()
+		{
+			int num = MaximumSwap(2736);
+		}
+
+		public int MaximumSwap(int num)
+		{
+			char[] chNums = num.ToString().ToCharArray();
+			int iLen = chNums.Length;			
+
+			// 앞에서부터 숫자가 내림차순이 아닌 부분이 있었는지 check
+			bool isDescending = true;
+			for(int i = 0; i < iLen - 1; i++)
+			{
+				if(chNums[i] < chNums[i + 1])
+				{
+					isDescending = false;
+					break;
+				}
+			}
+			
+			if (!isDescending)
+			{
+				int iMaxIdx = -1;
+				int iMinIdx = -1;
+				char chTemp;
+
+				// 높은 값 순서대로 정렬해놓고
+				char[] chMaxs = chNums.Distinct().OrderByDescending(n => n).ToArray();
+				int iMaxUsed = 0;
+				while(true)
+				{
+					// 높은값이 속해 있는 index를 찾아봐 뒤에서부터..
+					for (int i = iLen - 1; i >= 0; i--)
+					{
+						if(chNums[i] == chMaxs[iMaxUsed])
+						{
+							iMaxIdx = i;
+							break;
+						}
+					}
+				
+					// 앞에서부터 높은값보다 작은 수를 찾아서
+					for(int i = 0; i <= iMaxIdx; i++)
+					{
+						if(chNums[i] < chMaxs[iMaxUsed])
+						{
+							iMinIdx = i;
+							break;
+						}
+					}
+				
+					// 찾았으면 Swap
+					if(iMaxIdx != -1 && iMinIdx != -1)
+					{
+						chTemp = chNums[iMaxIdx];
+						chNums[iMaxIdx] = chNums[iMinIdx];
+						chNums[iMinIdx] = chTemp;
+						break;
+					}
+
+					// 없었으면 다음의 높은수로
+					iMaxUsed++;
+				}
+			}
+
+			return int.Parse(chNums);
+		}
+	}
+}
---
+++ Q/1200_1299/Quest_1233.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Q
+{
+	/**
+	 * 폴더 목록 폴더가 주어지면 해당 폴더의 모든 하위 폴더를 제거한 후 폴더를 반환합니다. 
+	 * 어떤 순서로든 답을 반환할 수 있습니다. 
+	 * 폴더[i]가 다른 폴더[j] 안에 있는 경우, 이를 폴더의 하위 폴더라고 합니다. 
+	 * 폴더[j]의 하위 폴더는 폴더[j]로 시작하고 그 뒤에 "/"가 와야 합니다. 
+	 * 예를 들어 "/a/b"는 "/a"의 하위 폴더이지만 "/b"는 "/a/b/c"의 하위 폴더가 아닙니다. 
+	 * 경로의 형식은 하나 이상의 연결된 문자열 형식입니다: '/' 뒤에 하나 이상의 영문 소문자가 오는 형식입니다. 
+	 * 예를 들어, "/leetcode" 및 "/leetcode/problems"는 유효한 경로이지만 빈 문자열과 "/"는 유효하지 않습니다.
+	 */
+
+	/*
+	 * 114ms Runtime : Beats 75.00%
+	 * 80.52MB Memory : Beats 62.50%
+	*/
+	internal class Quest_1233 : Quest
+	{
+		//public override void Init()
+		//{			
+		//}
+
+		public IList<string> RemoveSubfolders(string[] folder)
+		{
+			Array.Sort(folder);
+			HashSet<string> hashFolder = new HashSet<string>(); // return해야 할 folder list
+			int iFolderLen = folder.Length;
+			int iNameLen = 0;
+			StringBuilder sb = new StringBuilder();
+			for(int i = 0; i < iFolderLen; i++)
+			{
+				sb.Clear();
+				iNameLen = folder[i].Length;
+				char[] chName = folder[i].ToCharArray();
+				for(int j = 0; j < iNameLen; j++)
+				{
+					sb.Append(chName[j]);
+					if(((j < iNameLen - 1 && chName[j + 1] == '/') || (j == iNameLen - 1)))
+					{
+						if(hashFolder.Contains(sb.ToString()))
+							break;
+						else
+						{
+							if(j == iNameLen - 1) // 폴더 이름의 끝까지 봤는데 hash에 없으면 추가
+							{
+								hashFolder.Add(sb.ToString());
+							}
+						}
+					}
+				}
+			}
+			return hashFolder.ToList();
+		}
+	}
+}
